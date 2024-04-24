5cf193e6a1dff44f0b644409e3d509e379eeff57
Momenuzzaman <mdeamon4@gmail.com>
2024-04-24 13:11:07 +0600

    our first commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,5 @@
-
-
 // export const rootUrl = `http://api.uapp.uk/`
 
+// export const rootUrl = `http://192.168.0.14:45457/`
 
-export const rootUrl = `http://192.168.0.14:45456/`
-
-
-
+export const rootUrl = `http://localhost:8080/`;
